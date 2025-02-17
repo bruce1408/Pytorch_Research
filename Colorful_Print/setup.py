5340a94ec4013fa56a277bd08fe5042bdb6a9a31
bruce1408 <summer56567@163.com>
2025-02-17 19:21:48 +0800

    feat(printk): 支持多线程/进程安全打印

--- Colorful_Print/setup.py
+++ Colorful_Print/setup.py
@@ -14,7 +14,7 @@ setup(
     # package information
     
     name="printk",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     description="A colorful print tool for python",
     author="bruce_cui",
