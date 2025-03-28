cba2d2e4afff13ca33840e8c12f01a042dc0cb8a
bruce1408 <summer56567@163.com>
2025-03-28 18:48:02 +0800

    refactor(test_onnx): Update test script to load and visualize PyTorch model weights, enhancing functionality for model comparison and visualization clarity.

--- SpectraUtils/test/test_onnx.py
+++ SpectraUtils/test/test_onnx.py
@@ -6,19 +6,12 @@ from  torchvision.models import ResNet18_Weights
 
 if __name__ == '__main__':
     
+    # 加载onnx模型
     onnx_path = "/share/cdd/onnx_models/od_bev_0317.onnx"
     model_name = "od_bev_test"
-    # results_dir = "/mnt/share_disk/bruce_trie/workspace/Pytorch_Research/SpectraUtils/od_bev_onnx_visualization_results"
     visualize_onnx_model_weights(onnx_path, model_name)
     
-    # Example usage with different models
-    # model_old = torchvision.models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
-    
-    # 加载你的本地模型
-    # model_new = torch.load('/mnt/share_disk/bruce_trie/workspace/Pytorch_Research/SpectraUtils/spectrautils/resnet_model_cle_bc.pt')
-
-    # model_new = torchvision.models.resnet18(pretrained=False)
-
-    
-    # visualize_torch_model_weights(model_new, "resnet18_new")
+    # 加载torch模型
+    model_new = torch.load('/mnt/share_disk/bruce_trie/workspace/Pytorch_Research/SpectraUtils/spectrautils/resnet_model_cle_bc.pt')
+    visualize_torch_model_weights(model_new, "resnet18_new")
     
