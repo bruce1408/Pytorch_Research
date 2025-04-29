c9f28cbcded705c8784fc9a972f636457fdfb6e3
bruce1408 <summer56567@163.com>
2025-04-30 00:56:50 +0800

    新增(onnx_export): 在ResNet50模型中添加ONNX导出功能，更新模型初始化和输入形状，优化代码注释以提高可读性。

--- Torch_Experiments/CV/models/Classify/1_1_8_ResNet_v1.py
+++ Torch_Experiments/CV/models/Classify/1_1_8_ResNet_v1.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from torch.onnx import export
+
 # from torchsummary import summary
 """
 实现pytorch ResNet50网络结构,实现连接就是不用加卷积的情况
@@ -20,13 +22,19 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.dim = dim
         self.downsampling = downsampling
+        
         self.bottleneck = nn.Sequential(
+            # 1x1卷积，降维，权重是 out_channel x in_channel x h x w, W * x, 所以 in_channel 在后面
             nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=1),  # 默认padding=0
+            # 批归一化
             nn.BatchNorm2d(out_channel),
+            # ReLU激活
             nn.ReLU(),
+            # 3x3卷积
             nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(out_channel),
             nn.ReLU(),
+            # 1x1卷积升维
             nn.Conv2d(out_channel, out_channel*dim, kernel_size=1, stride=1),
             nn.BatchNorm2d(out_channel*dim)
         )
@@ -55,15 +63,17 @@ class Bottleneck(nn.Module):
 class ResNet50(nn.Module):
     def __init__(self, blocks, num_classes=2):
         super(ResNet50, self).__init__()
+        
+        # 初始卷积层
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 64, 7, 2, 3),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(3, 2)
         )  # output shape=[55, 55, 3]
         # blocks = [3, 4, 6, 3]
 
-        # 4个block进行操作
+        # 4个残差块
         self.layer2 = self.make_layer(64, 64, blocks[0], stride=1)
         self.layer3 = self.make_layer(256, 128, blocks[1], stride=2)
         self.layer4 = self.make_layer(512, 256, blocks[1], stride=2)
@@ -97,14 +107,25 @@ class ResNet50(nn.Module):
 
 
 if __name__ == "__main__":
-    x = torch.randn(2, 3, 224, 224)
-    # net = ResNet50([3, 4, 6, 3])
-    # # if torch.cuda.is_available():
-    # #     summary(net.cuda(), (3, 224, 224))
-    # # else:
-    # #     summary(net, (3, 224, 224))
-    # output = net(x)
+    x = torch.randn(1, 3, 224, 224)
+    model = ResNet50([3, 4, 6, 3], num_classes=2)
+    model.eval()
+    # if torch.cuda.is_available():
+    #     summary(net.cuda(), (3, 224, 224))
+    # else:
+    #     summary(net, (3, 224, 224))
+    outputs = model(x)
 
-    net1 = Bottleneck(3, 64)
-    outputs = net1(x)
+    # net1 = Bottleneck(3, 64)
+    # outputs = net1(x)
     print(outputs.shape)
+    
+    export(
+        model,
+        x,
+        "./resnet50_manual_build.onnx",
+        export_params=True,
+        do_constant_folding=True,
+        input_names=["input"],
+        output_names=["output"],
+    )
