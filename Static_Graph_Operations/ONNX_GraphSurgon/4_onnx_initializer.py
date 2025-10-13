56d3a6eb9c1feb63360be44a2f6a669f72ae9d4c
bruce1408 <summer56567@163.com>
2025-10-13 20:24:33 +0800

    更新ONNX图操作文件以使用新的模型路径，并优化初始化器和节点信息的输出格式，增加对tabulate库的支持以提升可读性。

--- Static_Graph_Operations/ONNX_GraphSurgon/4_onnx_initializer.py
+++ Static_Graph_Operations/ONNX_GraphSurgon/4_onnx_initializer.py
@@ -1,11 +1,44 @@
 import onnx
-
+from tabulate import tabulate
 # 加载 ONNX 模型
-model = onnx.load("/mnt/share_disk/bruce_trie/onnx_models/od_bev_1110.onnx")
+onnx_path = "/mnt/share_disk/bruce_trie/workspace/Quantizer-Tools/_outputs/models/resnet18.onnx"
+model = onnx.load(onnx_path)
 
-# 获取模型的initializer
+# 获取模型的initializer 初始化值
 initializers = model.graph.initializer
 
+initializers_list = {
+    "name": [],
+    "dims": [],
+    "data_type": []
+}
+
 for init in initializers:
-    print(init.name)
-    print(init.dims)  # 查看张量的形状
+    initializers_list["name"].append(init.name)
+    initializers_list["dims"].append(init.dims)
+    initializers_list["data_type"].append(init.data_type)
+
+print(tabulate(initializers_list, headers="keys", tablefmt="fancy_grid"))
+
+print("="*100)
+# exit(0)
+
+node_list = {
+    "name": [],
+    "op_type": [],
+    # "input": [],
+    # "output": [],
+    # "attribute": [],
+    # "domain": [],
+    # "doc_string": []
+}
+for node in model.graph.node:
+    node_list["name"].append(node.name)
+    node_list["op_type"].append(node.op_type)
+    # node_list["input"].append(node.input)
+    # node_list["output"].append(node.output)
+    # node_list["attribute"].append(node.attribute)
+    # node_list["domain"].append(node.domain)
+    # node_list["doc_string"].append(node.doc_string)
+    
+print(tabulate(node_list, headers="keys", tablefmt="fancy_grid"))
