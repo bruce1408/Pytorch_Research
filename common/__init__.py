61b98b3c9e28a98d85a2fe0a07d9facf345d44ed
bruce1408 <summer56567@163.com>
2025-10-13 11:51:52 +0800

    新增ONNX导出功能，支持在模型转换前后导出模型至ONNX格式，并优化了ReLU到SiLU的转换逻辑。同时，添加了workspace的导入以便于环境管理。

---
+++ common/__init__.py
@@ -0,0 +1,4 @@
+from .workspace import enter_workspace
+from .config import config_operator, config_onnx_inference, config_spectrautils
+
+__all__ = ["enter_workspace", "config_operator", "config_onnx_inference", "config_spectrautils"]
